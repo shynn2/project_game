7396913ddb732dd664034153ab25cbae377d4bd3
shynn2 <zkxkflsk427a@gmail.com>
2025-12-04 01:23:52 +0900

    Update draw.h

--- src/draw.h
+++ src/draw.h
@@ -7,7 +7,7 @@ void ClearWindow(App *app);
 void ShowWindow(App *app);
 
 // [수정] 인자에 Game Over 텍스트들 추가
-void DrawGame(App *app, TextObject *score, TextObject *life, TextObject *go, TextObject *restart);
+void DrawGame(App *app, TextObject *score, TextObject *go, TextObject *restart);
 
 void RenderEntity(App *app, Ingredient *ing);
 void RenderScoreBoard(App *app, TextObject *text_obj);
