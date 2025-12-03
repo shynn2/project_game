c8b60196b66de720c3d6123e6600edcfd0d069b0
shynn2 <zkxkflsk427a@gmail.com>
2025-12-03 23:00:07 +0900

    Update main.h

--- src/main.h
+++ src/main.h
@@ -6,19 +6,13 @@
 #include "input.h"
 #include "action.h"
 #include "draw.h"
-#include "sound.h"
-
-App app;
-TextObject score_text;
-
-TextObject health_text;
-
-char text_buffer[BUFSIZE];
 
 // 전역 변수 선언 (defs.h의 App 구조체 사용)
 // main.c에서 정의될 변수를 다른 파일들이 참조할 수 있게 합니다.
 extern App app;
 
 #endif
+
+
 
 
